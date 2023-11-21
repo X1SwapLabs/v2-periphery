603237996b81f79fd4298defe4485d9f8a270de0
magj2006 <magj2006@gmail.com>
2023-11-21 21:23:33 +0800

    chore: forge init

---
+++ src/interfaces/IX1SwapV2Migrator.sol
@@ -0,0 +1,6 @@
+pragma solidity >=0.5.0;
+
+interface IX1swapV2Migrator {
+    function migrate(address token, uint256 amountTokenMin, uint256 amountETHMin, address to, uint256 deadline)
+        external;
+}
